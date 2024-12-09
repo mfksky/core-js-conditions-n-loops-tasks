289fa7443f1284cb589f12267680f25f98c59c5c
Alex <aleksa1904@icloud.com>
2024-12-09 22:50:16 +0300

    feat: complete 11 tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -89,8 +89,9 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a + b <= c || a + c <= b || b + c <= a) return false;
+  return a === b || a === c || b === c;
 }
 
 /**
@@ -107,8 +108,50 @@ function isIsoscelesTriangle(/* a, b, c */) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  const test = [
+    'I',
+    'II',
+    'III',
+    'IV',
+    'V',
+    'VI',
+    'VII',
+    'VIII',
+    'IX',
+    'X',
+    'XI',
+    'XII',
+    'XIII',
+    'XIV',
+    'XV',
+    'XVI',
+    'XVII',
+    'XVIII',
+    'XIX',
+    'XX',
+    'XXI',
+    'XXII',
+    'XXIII',
+    'XXIV',
+    'XXV',
+    'XXVI',
+    'XXVII',
+    'XXVIII',
+    'XXIX',
+    'XXX',
+    'XXXI',
+    'XXXII',
+    'XXXIII',
+    'XXXIV',
+    'XXXV',
+    'XXXVI',
+    'XXXVII',
+    'XXXVIII',
+    'XXXIX',
+  ];
+
+  return test[num - 1];
 }
 
 /**
@@ -126,8 +169,61 @@ function convertToRomanNumerals(/* num */) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(numberStr) {
+  let result = '';
+
+  for (let i = 0; i < numberStr.length; i += 1) {
+    const num = numberStr[i];
+    let str = '';
+
+    switch (num) {
+      case '-':
+        str = 'minus';
+        break;
+      case ',':
+        str = 'point';
+        break;
+      case '.':
+        str = 'point';
+        break;
+      case '0':
+        str = 'zero';
+        break;
+      case '1':
+        str = 'one';
+        break;
+      case '2':
+        str = 'two';
+        break;
+      case '3':
+        str = 'three';
+        break;
+      case '4':
+        str = 'four';
+        break;
+      case '5':
+        str = 'five';
+        break;
+      case '6':
+        str = 'six';
+        break;
+      case '7':
+        str = 'seven';
+        break;
+      case '8':
+        str = 'eight';
+        break;
+      case '9':
+        str = 'nine';
+        break;
+      default:
+        break;
+    }
+
+    if (str) result += (result ? ' ' : '') + str;
+  }
+
+  return result;
 }
 
 /**
@@ -142,8 +238,17 @@ function convertNumberToString(/* numberStr */) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let result = true;
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] !== str[str.length - i - 1]) {
+      result = false;
+      break;
+    }
+  }
+
+  return result;
 }
 
 /**
@@ -160,8 +265,14 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      return i;
+    }
+  }
+
+  return -1;
 }
 
 /**
@@ -179,8 +290,17 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const str = String(num);
+  const digitStr = String(digit);
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === digitStr) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 /**
@@ -196,8 +316,24 @@ function isContainNumber(/* num, digit */) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  let total = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    total += arr[i];
+  }
+
+  let left = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    const right = total - left - arr[i];
+
+    if (right === left) return i;
+
+    left += arr[i];
+  }
+
+  return -1;
 }
 
 /**
